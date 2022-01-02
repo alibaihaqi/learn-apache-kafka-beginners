b827cb6ba0fad7de7dfccb35bf41f5380344b7f4
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2022-01-02 22:24:05 +0700

    refactor: refactor to module kafka-basics

--- kafka-basics/src/main/java/kafka/tutorial1/ConsumerDemoGroups.java
+++ kafka-basics/src/main/java/kafka/tutorial1/ConsumerDemoGroups.java
@@ -1,4 +1,4 @@
-package com.alibaihaqi.kafka.tutorial1;
+package kafka.tutorial1;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -12,13 +12,13 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
-public class ConsumerDemo {
+public class ConsumerDemoGroups {
 
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
+        Logger logger = LoggerFactory.getLogger(ConsumerDemoGroups.class.getName());
 
         String bootstrapServers = "127.0.0.1:9092";
-        String groupId = "my-fourth-application";
+        String groupId = "my-fifth-application";
         String topic = "first_topic";
 
         Properties properties = new Properties();
