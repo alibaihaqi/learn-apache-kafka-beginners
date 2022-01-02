1c021751f798b49170f1355ec64717fb796587d9
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2022-01-02 23:21:53 +0700

    refactor: refactor package structure

--- kafka-basics/src/main/java/com/alibaihaqi/kafka/tutorial1/ConsumerDemo.java
+++ kafka-basics/src/main/java/com/alibaihaqi/kafka/tutorial1/ConsumerDemo.java
@@ -1,4 +1,4 @@
-package kafka.tutorial1;
+package com.alibaihaqi.kafka.tutorial1;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -12,13 +12,13 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
-public class ConsumerDemoGroups {
+public class ConsumerDemo {
 
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(ConsumerDemoGroups.class.getName());
+        Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
 
         String bootstrapServers = "127.0.0.1:9092";
-        String groupId = "my-fifth-application";
+        String groupId = "my-fourth-application";
         String topic = "first_topic";
 
         Properties properties = new Properties();
--- kafka-basics/src/main/java/com/alibaihaqi/kafka/tutorial1/ProducerDemoWithCallback.java
+++ kafka-basics/src/main/java/com/alibaihaqi/kafka/tutorial1/ProducerDemoWithCallback.java
@@ -1,4 +1,4 @@
-package kafka.tutorial1;
+package com.alibaihaqi.kafka.tutorial1;
 
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
